85d68da589fe216b13246744c679fabb88551f8b
Noor Abu Zakari <noor.abuzakari01@gmail.com>
2022-09-20 11:31:08 +0300

    first commit

---
+++ Assignemnt 2.py
@@ -0,0 +1,16 @@
+#define integer value with legal name.
+num = 10
+print(num)
+#define string value with legal name
+val= "noor"
+print(val)
+#define string value contains a float number then cast it to float and store the new type in another variable
+val2 = "1.2"
+val3 = float(val2)
+print(val3)
+#cast boolean variable to integer type.
+val4 = True
+val5 = int(val4)
+print(val5)
+#print all variables which you declared above line by line
+
